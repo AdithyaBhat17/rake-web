a0a4b07769db48fca241177d889955448b9bfc26
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2018-10-02 18:16:14 +0530

    Landing page hero section is ready TODO : Make it responsive for tabs and add mockups

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import './App.css';
+import Home from './Home';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <p>
-          Coming soon
-        </p>
+      <div>
+        <Home />
       </div>
     );
   }
---
+++ src/Home/Nav.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default function Nav() {
+    return (
+        <nav className="navbar navbar-default">
+            <div className="container">
+                <div className="navbar-header">
+                <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span className="sr-only">Toggle navigation</span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                </button>
+                <a className="navbar-brand" href="#">rake</a>
+                </div>
+                <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul className="nav navbar-nav navbar-right">
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Login</a></li>
+                        {/* <li><a className="get-app"  href="#">Get App</a></li> */}
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
