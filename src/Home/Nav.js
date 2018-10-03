0dfb75fd645061b75089df22a2b89158c30b9752
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2018-10-03 19:23:06 +0530

    Changed font colors

--- src/Home/Nav.js
+++ src/Home/Nav.js
@@ -5,18 +5,18 @@ export default function Nav() {
         <nav className="navbar navbar-default">
             <div className="container">
                 <div className="navbar-header">
-                <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <button type="button" className="navbar-toggle collapsed animated fadeInDown" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                     <span className="sr-only">Toggle navigation</span>
                     <span className="icon-bar"></span>
                     <span className="icon-bar"></span>
                     <span className="icon-bar"></span>
                 </button>
-                <a className="navbar-brand" href="#">rake</a>
+                <a className="navbar-brand animated fadeInDown" href="/">rake</a>
                 </div>
-                <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul className="nav navbar-nav navbar-right">
-                        <li><a href="#">About Us</a></li>
-                        <li><a href="#">Login</a></li>
+                <div className="collapse navbar-collapse animated fadeInDown" id="bs-example-navbar-collapse-1">
+                    <ul className="nav navbar-nav navbar-right animated fadeIn">
+                        <li><a href="/">About Us</a></li>
+                        <li><a href="/">Login</a></li>
                         {/* <li><a className="get-app"  href="#">Get App</a></li> */}
                     </ul>
                 </div>
--- src/Home/index.js
+++ src/Home/index.js
@@ -10,17 +10,17 @@ class Home extends Component {
         <div className="container">
             <div className="row">
               <div className="col-lg-6 col-lg-push-6">
-                <img src={hero} className="hero" alt="rake"/>
+                <img src={hero} className="hero animated fadeInRight" alt="rake"/>
               </div>
               <div className="col-lg-6 col-lg-pull-6">
-                <h1 className="tagline">Make your images smart</h1>
-                <p className="about-site">Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                <h1 className="tagline animated fadeInLeft">Make your images smart</h1>
+                <p className="about-site animated fadeInLeft">Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
                   Totam commodi ut quisquam nobis aliquam minima deserunt provident atque illum quam nisi eius 
                   voluptate reiciendis aspernatur ducimus quae dicta labore ipsam, nemo nihil dignissimos harum 
                   dolores amet. Numquam odit optio delectus laboriosam quo. Assumenda nam aperiam provident quas 
                   architecto, voluptatibus expedita.
                   <br/> <br/>
-                  <a className="download-app">Download App</a>
+                  <a href="/" className="download-app"><i className="fab fa-google-play"></i>&nbsp; GET APP</a>
                 </p>
               </div>
             </div>
