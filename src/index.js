6c4c7ae3c40516915340512b689c9cd66795d336
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2018-10-05 22:52:42 +0530

    Feat : Implemented react-router-dom

--- src/index.js
+++ src/index.js
@@ -2,9 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
 // import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <React.Fragment>
+            <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/>
+        </React.Fragment>
+    </Router>,
+ document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
