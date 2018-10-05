2446dcc33da5b354e2d61d6f584160eba8062811
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2018-10-05 22:52:13 +0530

    Refactored to be used for dashboard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-import Home from './Home';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Home />
       </div>
     );
   }
