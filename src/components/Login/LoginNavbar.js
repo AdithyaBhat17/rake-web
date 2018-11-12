8ae31da92c44599c9c8322f5ef0a09c777764b1d
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2018-11-13 01:40:46 +0530

    fadein animations

--- src/components/Login/LoginNavbar.js
+++ src/components/Login/LoginNavbar.js
@@ -8,17 +8,17 @@ export default class LoginNavbar extends React.Component{
             <nav className="navbar navbar-default">
                 <div className="container">
                     <div className="navbar-header">
-                    <button type="button" className="navbar-toggle collapsed animated fadeInDown" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <button type="button" className="navbar-toggle collapsed animated fadeIn" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                         <span className="sr-only">Toggle navigation</span>
                         <span className="icon-bar"></span>
                         <span className="icon-bar"></span>
                         <span className="icon-bar"></span>
                     </button>
-                    <Link className="navbar-brand animated fadeInDown" to="/" >
+                    <Link className="navbar-brand animated fadeIn" to="/" >
                         <img className="logo" src={logo} alt="rake"/>
                     </Link>
                     </div>
-                    <div className="collapse navbar-collapse animated fadeInDown" id="bs-example-navbar-collapse-1">
+                    <div className="collapse navbar-collapse animated fadeIn" id="bs-example-navbar-collapse-1">
                         <ul className="nav navbar-nav navbar-right animated fadeIn">                        
                             <li><Link className="lognav" to="/">Home</Link></li>
                             <li><Link className="lognav" to="/about">About Us</Link></li>         
