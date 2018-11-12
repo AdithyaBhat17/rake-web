507c5435958182e4cedfa9473a04a504c4fe7897
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2018-11-13 01:40:34 +0530

    fadein animations

--- src/components/Home/Nav.js
+++ src/components/Home/Nav.js
@@ -7,17 +7,17 @@ export default function Nav() {
         <nav className="navbar navbar-default">
             <div className="container">
                 <div className="navbar-header">
-                <button type="button" className="navbar-toggle collapsed animated fadeInDown" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <button type="button" className="navbar-toggle collapsed animated fadeIn" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                     <span className="sr-only">Toggle navigation</span>
                     <span className="icon-bar"></span>
                     <span className="icon-bar"></span>
                     <span className="icon-bar"></span>
                 </button>
-                <a className="navbar-brand animated fadeInDown" href="/">
+                <a className="navbar-brand animated fadeIn" href="/">
                     <img className="logo" src={logo} alt="rake"/>
                 </a>
                 </div>
-                <div className="collapse navbar-collapse animated fadeInDown" id="bs-example-navbar-collapse-1">
+                <div className="collapse navbar-collapse animated fadeIn" id="bs-example-navbar-collapse-1">
                     <ul className="nav navbar-nav navbar-right animated fadeIn">                        
                         <li><Link to="/about">About Us</Link></li>
                         <li><Link to="/login">Login</Link></li>         
