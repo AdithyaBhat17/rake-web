5f1baa445695ee042e2fdf756a417ceb08d0b42d
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2018-11-13 01:39:58 +0530

    Feat: Signup route

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Login from './components/Login';
+import Signup from './components/Login/Signup';
 // import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
@@ -14,6 +15,7 @@ ReactDOM.render(
             <Route exact path="/" component={Home}/>
             <Route path="/about" component={About}/>
             <Route path="/login" component={Login}/>
+            <Route path="/join" component={Signup}/>
         </React.Fragment>
     </Router>,
  document.getElementById('root'));
