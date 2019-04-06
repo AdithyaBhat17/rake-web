32d7ef8b7030785056e3f7753cef9b33140046a2
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-04-07 00:34:18 +0530

    :zap: Changed api and testing out fetching data

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,25 +1,43 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
 
-import firebase from '../../Firebase'
+import firebase, { db } from '../../Firebase'
 import NavbarDash from './NavbarDash'
 
-const logOut = () => {
+const logOut = (props) => {
     try {
         firebase
         .auth()
         .signOut()
-        .then(() => this.props.history.push('/'))
+        .then(() => props.history.push('/'))
     } catch (err) {
         alert(err)
     }
 }
 
-const Dashboard = ({user}) => {
+const Dashboard = (props) => {
+    let [data, setData] = React.useState([])
+    React.useEffect(() => {
+        const fetchUserData = () => {
+            // TEST FOR TEXT RECOGNITION
+            db.collection('users').doc(props.user[1] || props.user.uid).collection('recognizedText').get()
+            .then(async collection => {
+                let i = collection.docs.map(doc => doc.data())        
+                await setData(i)
+                console.log(data)
+            })
+        }
+
+        fetchUserData()
+        console.log(data)
+    }, [JSON.stringify(data)])
+
+    const { user } = props
+
     console.log(user.displayName)
     return(
         <div>
-            <NavbarDash logOut={logOut}/>
+            <NavbarDash logOut={() => logOut(props)}/>
             <div className="container-fluid dashboard">
                     <div className="row">
                     <div className="col-md-3 col-sm-12">
