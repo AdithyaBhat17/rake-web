5ac1cb91a608338a3934dbe7ed24c42b9c9afa34
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-04-10 22:22:15 +0530

    :zap: make firebase config public

---
+++ src/Firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase'
+
+const config = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: "",
+    messageSenderId: process.env.REACT_APP_SENDER_ID
+}
+
+firebase.initializeApp(config)
+
+export const db = firebase.firestore()
+
+export const provider = new firebase.auth.GoogleAuthProvider()
+
+export default firebase
