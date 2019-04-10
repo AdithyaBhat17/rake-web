9d9456747079a23aa0aa763cc7799f109999db36
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-04-10 22:31:30 +0530

    :zap: FIX: productId is now a string

--- src/Firebase.js
+++ src/Firebase.js
@@ -4,7 +4,7 @@ const config = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
+    projectId: process.env.REACT_APP_PROJECT_ID.toString(),
     storageBucket: "",
     messageSenderId: process.env.REACT_APP_SENDER_ID
 }
