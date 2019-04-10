1ca6de27655381cfbc8979cb30533f0cb3d2dd12
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-04-10 22:36:25 +0530

    :wrench: log config details

--- src/Firebase.js
+++ src/Firebase.js
@@ -4,10 +4,12 @@ const config = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID.toString(),
+    projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: "",
     messageSenderId: process.env.REACT_APP_SENDER_ID
 }
+
+console.log(process.env)
 
 firebase.initializeApp(config)
 
