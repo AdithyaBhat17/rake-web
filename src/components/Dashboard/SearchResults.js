50d25f0bd50354f05117ae8dafd435661bea3841
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-04-20 23:02:13 +0530

    :sparkles: render dialog box contents

---
+++ src/components/Dashboard/SearchResults.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const getLinkTitle = (link) => {
+    return link.substring(link.indexOf('.') + 1, link.indexOf('.', link.indexOf('.') + 1))
+}
+
+const SearchResults = ({data}) => {
+    return (
+        <div style={{opacity: 0.9}}>
+            <p>Best Guess: <strong style={{textTransform: 'capitalize'}}>{data.best_guess}</strong></p>
+            <p>{data.descriptions[0].substring(0, data.descriptions[0].indexOf('.'))}.</p>
+            <p style={{display: 'inline-flex'}}>
+                More info: &nbsp; {data.links.map((link, index) => (
+                    <span className="label" key={index}>
+                        <a style={{color: '#2522a6'}} href={link} target="_blank" rel="noopener noreferrer">{getLinkTitle(link)}</a>
+                    </span>
+                ))}
+            </p>
+            <p><a href={data.similar_images[0]} target="_blank" rel="noopener noreferrer" className="similar">Similar Images</a></p>
+        </div>
+    )
+}
+
+export default SearchResults
