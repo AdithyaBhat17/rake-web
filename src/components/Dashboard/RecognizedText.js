07083f0799ff4cf096bd56a375438f320251c18f
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-04-22 21:10:34 +0530

    :sparkles: formatted timestamp using momentjs

--- src/components/Dashboard/RecognizedText.js
+++ src/components/Dashboard/RecognizedText.js
@@ -2,8 +2,16 @@ import React from 'react'
 import { db } from '../../Firebase'
 import NavbarDash from './NavbarDash'
 import { logOut } from './index'
+import { Link } from 'react-router-dom'
+import moment from 'moment'
 
 import { SemipolarSpinner } from 'react-epic-spinners'
+
+const formatTimestamp = (timestamp) => {
+    const date = moment(timestamp).format("dddd, MMMM Do YYYY, h:mm a")
+    const formattedTimestamp = date.split(',')
+    return formattedTimestamp[0] + ', ' + formattedTimestamp[1] + ' at ' + formattedTimestamp[2]
+}
 
 const RecognizedText = (props) => {
     let [data, setData] = React.useState(null)
@@ -33,6 +41,26 @@ const RecognizedText = (props) => {
     return (
         <div>
             <NavbarDash logOut={() => logOut(props)} />
+            <div className="container">
+                <div className="row">
+                    {data && data.map(text => (
+                        <div key={text.id} className="col-md-4 col-sm-12">
+                            <div className="thumbnail timestamp">
+                                <Link
+                                 style={{color: '#2522a6'}} 
+                                 to={`/dashboard/recognize-text/${text.timestamp}`}>
+                                 <small>{formatTimestamp(text.timestamp)}</small>
+                                </Link>
+                                <Link
+                                 style={{color: '#2522a6'}} 
+                                 to={`/dashboard/recognize-text/${text.timestamp}`}>
+                                 ->
+                                </Link>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
