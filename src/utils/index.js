6048f6ab027af333c1766983418da4e545e02422
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-04-24 21:45:30 +0530

    :zap: changed api endpoint url

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,5 @@
 export default function searchAPI(body) {
-    return fetch("https://cors-anywhere.herokuapp.com/https://final-ris-app.herokuapp.com/search", {
+    return fetch("https://cors-anywhere.herokuapp.com/https://ris-app.herokuapp.com/search", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
