9c097964ac67201a61888a6c1b0275cb75313b94
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-05-03 20:47:39 +0530

    :sparkles: Fix: for issue #3

--- src/components/Dashboard/SearchResults.js
+++ src/components/Dashboard/SearchResults.js
@@ -9,14 +9,14 @@ const SearchResults = ({data}) => {
         <div style={{opacity: 0.9}}>
             <p>Best Guess: <strong style={{textTransform: 'capitalize'}}>{data.best_guess}</strong></p>
             <p>{data.descriptions[0].substring(0, data.descriptions[0].indexOf('.'))}.</p>
-            <p style={{display: 'inline-flex'}}>
+            <p>
                 More info: &nbsp; {data.links.map((link, index) => (
                     <span className="label" key={index}>
                         <a style={{color: '#2522a6'}} href={link} target="_blank" rel="noopener noreferrer">{getLinkTitle(link)}</a>
                     </span>
                 ))}
             </p>
-            <p>
+            <p style={{display: 'flex', textAlign: 'center'}}>
                 <a href={data.similar_images[0]} target="_blank" rel="noopener noreferrer" className="similar">Similar Images</a>
                 <a href={data.shop[0]} target="_blank" rel="noopener noreferrer" className="similar">Shop for {data.best_guess}</a>
             </p>
